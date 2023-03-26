e3855622295a39e63d901dc35661a239f59fbbab
soos88885 <55650250+soos88885@users.noreply.github.com>
2023-03-26 11:40:52 +0200

    Merge fixes

--- new_api/response_builder.py
+++ new_api/response_builder.py
@@ -2,6 +2,7 @@ import re
 import xml.etree.ElementTree as ET
 from xml.etree.ElementTree import Element, tostring
 
+#AUTH BUILDER
 def credentials(xml):
     tree = ET.fromstring(xml)
     cred_dic={'credentials':{}}
